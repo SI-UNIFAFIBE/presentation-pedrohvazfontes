8903474a1308bac9c233b29826b34dfd97a9960c
pedrohvazfontes <pedro.fontes@aluno.unifafibe.edu.br>
2021-03-02 23:04:08 +0000

    Feito novamente

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Ola, eu sou Pedro e more em Bebedouro-SP");
+    System.out.println("Ola, eu sou Pedro e moro em Bebedouro-SP");
   }
 }
